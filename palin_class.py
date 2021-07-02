2d01d05b1de213a277ccc9af1b4af2fc30c78059
Boks1 <msal40755@gmail.com>
2021-07-02 17:52:52 +0700

    import file to palindrome checker

---
+++ palin_class.py
@@ -0,0 +1,17 @@
+from main import word
+
+class Testing:
+    def __init__(self):
+        self.word = word
+    
+    def checking(self):
+        check_word = 0
+        len_word = len(self.word)
+        word_list = [letter.lower() for letter in self.word]
+        for i in range(len_word):
+            if word_list[i] == word_list[len_word-(i+1)]:
+                check_word += 1
+                if check_word == len_word - 1:
+                    return f"{word} is palindrome word."
+            else:
+                return f"{word} isn't palindrome word."
