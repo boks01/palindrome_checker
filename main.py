e3a12ad0fab693319f8ed273104c47341c6ed842
boks01 <msal40755@gmail.com>
2021-07-02 18:34:11 +0700

    deleting testing file and cleaning main.py

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-# from palin_class import Check
 from test import Test
 
-# process = Check()
 
 print("Palyndrome Word")
 word = input("type your word: ")
-# print(process.checking())
 test = Test(word)
 test_check = test.checking()
 print(test_check)
